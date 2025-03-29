959fabc2f28db6cd281c52cdfdf9eb1f2dc08f9d
Brett <brett.mckenzie3@gmail.com>
2025-03-29 13:26:32 -0700

    Fixed Ball movement and added a basic AI opponent

--- Assets/Goal.cs
+++ Assets/Goal.cs
@@ -2,14 +2,20 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
-    public ScoreManagement scoreManager;
+    public bool isPlayerGoal = false;
 
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ball"))
         {
-            // Notify ScoreManager that a goal was scored, passing this goal's Collider
-            scoreManager.OnGoalScored(gameObject);
+            if (isPlayerGoal)
+            {
+                GameManager.Instance.AIScores();
+            }
+            else
+            {
+                GameManager.Instance.PlayerScores();
+            }
         }
     }
 }
